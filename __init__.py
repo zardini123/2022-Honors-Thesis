48936a4f3374e821836e17a9af29bbdbd98fc919
zardini123 <tarasikpalcynski@gmail.com>
2022-09-28 17:36:25 -0400

    Setup Blender addon initalization and created "Add Bézier surface" menu item

---
+++ __init__.py
@@ -0,0 +1,92 @@
+"""2022 Honors Thesis Blender Addon."""
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+
+bl_info = {
+    "name": "2022 Honors Thesis",
+    "description": "Bézier surfaces with meshing",
+    "author": "Taras Palczynski III",
+    "version": (0, 0, 1),
+    "blender": (3, 3, 0),
+    "location": "View3D > Add > Mesh",
+    "warning": "",  # used for warning icon and text in addons panel
+    "doc_url": "https://github.com/zardini123/2022-Honors-Thesis",
+    "tracker_url": "https://github.com/zardini123/2022-Honors-Thesis/issues",
+    "support": "COMMUNITY",
+    "category": "Mesh",
+}
+
+def add_object(self, context):
+    scale_x = self.scale.x
+    scale_y = self.scale.y
+
+    verts = [
+        Vector((-1 * scale_x, 1 * scale_y, 0)),
+        Vector((1 * scale_x, 1 * scale_y, 0)),
+        Vector((1 * scale_x, -1 * scale_y, 0)),
+        Vector((-1 * scale_x, -1 * scale_y, 0)),
+    ]
+
+    edges = []
+    faces = [[0, 1, 2, 3]]
+
+    mesh1 = bpy.data.meshes.new(name="Mesh 1")
+    mesh2 = bpy.data.meshes.new(name="Mesh 2")
+
+    # https://docs.blender.org/api/current/bpy.types.Mesh.html#bpy.types.Mesh.from_pydata
+    mesh1.from_pydata(verts, edges, faces)
+    mesh2.from_pydata(verts, edges, faces)
+
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh1, operator=self)
+    object_data_add(context, mesh2, operator=self)
+
+
+class OBJECT_OT_add_bezier_surface(Operator, AddObjectHelper):
+    """Create a new Mesh Object"""
+    bl_idname = "surface.bezier_surface"
+    bl_label = "Add Bézier Surface"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale: FloatVectorProperty(
+        name="Scale",
+        default=(1.0, 1.0, 1.0),
+        subtype='TRANSLATION',
+        description="Scaling",
+    )
+
+    def execute(self, context):
+        add_object(self, context)
+        return {'FINISHED'}
+
+def add_bezier_surface_button(self, context):
+    self.layout.operator(
+        OBJECT_OT_add_bezier_surface.bl_idname,
+        text="Bézier Surface",
+        icon='SURFACE_NSURFACE')
+
+
+def register():
+    """Call to register is made when addon is enabled."""
+    bpy.utils.register_class(OBJECT_OT_add_bezier_surface)
+
+    # "Extending Menus" Blender API
+    #       https://docs.blender.org/api/current/bpy.types.Menu.html#extending-menus
+    # "VIEW3D_MT_surface_add determined by Python Blender UI
+    #       Source: https://github.com/blender/blender/blob/482d431bb6735e8206961bd1115d2be7e63572b1/release/scripts/startup/bl_ui/space_view3d.py#L2122
+    bpy.types.VIEW3D_MT_surface_add.append(add_bezier_surface_button)
+
+
+def unregister():
+    """Call to unregister is made when addon is disabled."""
+    bpy.utils.unregister_class(OBJECT_OT_add_bezier_surface)
+    bpy.types.VIEW3D_MT_surface_add.remove(add_bezier_surface_button)
+
+
+if __name__ == "__main__":
+    register()
